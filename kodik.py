899d966672ecb94585e4f4800b4b46f3964c27f2
egortr2020 <e.p.korostelev@yandex.ru>
2024-09-23 18:51:07 +0300

    c4, A

--- kodik.py
+++ kodik.py
@@ -3,3 +3,7 @@ a=int(input())
 b=int(input())
 print("Молодец, Ты справился с этим!!!")
 print(a-b)
+print("Хочешь узнать число больше нуля? (да/нет)")
+da=str(input())
+if da=="да":
+    print((a-b)>0)
